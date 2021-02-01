f48f560be7baa01deba7b69d1e886f7615f242c7
Elliott Vargo <vargoelliott@gmail.com>
2021-02-01 00:47:50 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,11 +1,14 @@
 class Song
-
+attr_accessor :name, :artist, :genre
 #class variables
 @@count = 0
 @@artists = []
 @@genres = []
 
 def initialize(name, artist, genre)
+  @name = name
+  @genre = genre 
+  @artist = artist
   @@count += 1
   if @@artists == []
   @@artists << artist
