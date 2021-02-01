0f99556f60d90ed151300d8dd2597c71627f1252
Elliott Vargo <vargoelliott@gmail.com>
2021-02-01 00:24:19 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,9 +3,10 @@ class Song
 @@count = 0
 @@artists = []
 @@genres = []
-def initialize(name, artist, song)
+def initialize(name, artist, genre)
 @@count += 1
 @@artists << artist
+@@g
 end
 
 def count
